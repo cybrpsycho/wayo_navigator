a4b4a1e78a337e1e736b6ebc667b613c9b4efeab
mxntell <muturimonty@gmail.com>
2023-07-09 19:35:12 +0300

    init: create branch, add gltf & scene boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,143 +1,109 @@
-import { useEffect, useRef, useState } from "react";
-import {
-  Group,
-  MOUSE,
-  Raycaster,
-  TOUCH,
-  Vector3,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
-} from "three";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Suspense, useEffect, useRef } from "react";
+import { DirectionalLightHelper } from "three";
+import { Canvas } from "@react-three/fiber";
 import {
   PerspectiveCamera,
-  OrbitControls,
   GizmoHelper,
   GizmoViewport,
-  Sphere,
   Stats,
-  Image,
-  Grid,
+  MapControls,
+  useGLTF,
+  useProgress,
+  Html,
+  Sky,
+  useHelper,
 } from "@react-three/drei";
 
 import "./App.css";
 
-export default function App() {
-  const mouseButtons = { LEFT: MOUSE.PAN, RIGHT: MOUSE.ROTATE };
-  const touches = { ONE: TOUCH.PAN, TWO: TOUCH.DOLLY_ROTATE };
+useGLTF.preload(modelUrl);
 
+export default function App() {
   return (
     <Canvas frameloop="demand">
-      <Grid
-        args={[100, 100]}
-        position={[0, 0, 0.1]}
-        rotation={[0, -Math.PI / 2, -Math.PI / 2]}
-        sectionSize={5}
-        sectionThickness={1.5}
-        fadeDistance={400}
-        infiniteGrid
-      />
-      <GizmoHelper alignment="top-right" children={<GizmoViewport />} />
+      <Stage showDebugOverlays />
 
-      <OrbitControls
-        makeDefault
-        panSpeed={1.5}
-        minDistance={50}
-        maxDistance={300}
-        enableRotate={false}
-        touches={touches}
-        mouseButtons={mouseButtons}
-      />
-
-      <PerspectiveCamera makeDefault position={[0, 0, 100]} fov={20} />
-
-      <Scene />
+      <Suspense fallback={<Progress />}>
+        <Scene />
+      </Suspense>
 
       <Stats />
     </Canvas>
   );
 }
 
-const Scene = () => {
-  // TODO: Find the appropriate types and remove 'any' keyword
-  const imagePlaneRef = useRef<Mesh>(null);
-  const sphereGroupRef = useRef<Group>(null);
-
-  const [isHovered, setIsHovered] = useState<boolean>(false);
-  const [isCameraMoving, setIsCameraMoving] = useState<boolean>(false);
-  const [mousePosition, setMousePosition] = useState<Vector3>(new Vector3());
-
-  const raycaster = new Raycaster();
-
-  const { camera } = useThree();
-
-  useFrame(({ mouse }) => {
-    if (isHovered) {
-      raycaster.setFromCamera(mouse, camera);
-
-      if (imagePlaneRef.current) {
-        let intersects = raycaster.intersectObject(imagePlaneRef.current);
-
-        if (intersects.length > 0) {
-          let point = intersects[0].point;
-          setMousePosition(new Vector3(point.x, point.y, 0));
-        }
-      }
-    }
-  });
-
-  // useEffect(() => {});
-
-  const onDoubleMouseClick = () => {
-    if (sphereGroupRef.current && !isCameraMoving) {
-      let geometry = new BoxGeometry(0.5, 0.5, 0.5);
-      let material = new MeshBasicMaterial();
-
-      const cube = new Mesh(geometry, material);
-      cube.position.copy(mousePosition);
-
-      sphereGroupRef.current.add(cube);
-    }
-  };
-
+const Stage = (props: { showDebugOverlays?: boolean }) => {
   return (
-    <scene>
-      <Image
-        url="./sarit_layout_01.svg"
-        scale={100}
-        ref={imagePlaneRef}
-        onPointerOver={() => setIsHovered(true)}
-        onPointerOut={() => setIsHovered(false)}
-        onDoubleClick={onDoubleMouseClick}
+    <>
+      <Sky />
+
+      <Lighting showHelpers={props.showDebugOverlays} />
+
+      {props.showDebugOverlays ? (
+        <GizmoHelper alignment="top-right" children={<GizmoViewport />} />
+      ) : (
+        <></>
+      )}
+
+      <MapControls
+        makeDefault
+        panSpeed={1.5}
+        minDistance={50}
+        maxDistance={300}
+        screenSpacePanning={false}
       />
 
-      <group ref={sphereGroupRef} />
-
-      <Sphere position={mousePosition} args={[0.3]} />
-    </scene>
+      <PerspectiveCamera makeDefault position={[100, 100, 100]} fov={20} />
+    </>
   );
 };
 
-class PathGraph {
-  private nodes: [] = [];
-  private edges: [] = [];
+const Lighting = (props: { showHelpers?: boolean }) => {
+  const lightARef = useRef<any>(null);
+  const lightBRef = useRef<any>(null);
 
-  constructor() {}
+  useHelper(lightARef, DirectionalLightHelper);
+  useHelper(lightBRef, DirectionalLightHelper);
 
-  addNode = () => {};
+  return (
+    <>
+      <directionalLight
+        ref={props.showHelpers ? lightARef : null}
+        position={[-10, 10, -10]}
+        intensity={0.1}
+      />
+      <directionalLight
+        ref={props.showHelpers ? lightBRef : null}
+        position={[10, 10, 0]}
+        intensity={0.2}
+      />
+      <ambientLight intensity={0.7} />
+    </>
+  );
+};
 
-  getNode = (nodeId: string) => {};
+const Progress = () => {
+  const { progress } = useProgress();
 
-  addEdge = () => {};
+  return <Html center>{progress} % loaded</Html>;
+};
 
-  getPath = (startId: string, destinationId: string) => {};
-}
+const Scene = () => {
+  const { scene } = useGLTF(modelUrl);
 
-class Node {
-  id: string;
+  useEffect(() => {
+    console.log(scene);
+    // TODO
+    /*  split scene into navmesh and other objects i.e rooms
+        store data structures into a state variable
+        use navmesh to do pathfinding (3-pathfinding) - find out how to obtain mesh id using custom events
+         
+    */
+  });
 
-  constructor(id: string) {
-    this.id = id;
-  }
-}
+  return (
+    <scene>
+      <primitive object={scene} />
+    </scene>
+  );
+};
